f5eed4817a9ddc9874789342729b1c70c9312145
Daniel Killough <dkillough@utexas.edu>
2022-11-23 07:32:59 -0600

    prototype rendering appropriate descriptions

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,23 +1,28 @@
 let timecodeArr = [];
+const descriptionChanged = new CustomEvent('desc-changed')
 
 window.addEventListener("load", function () {
     console.log("window loaded");
     processData()
+    console.log(timecodeArr)
     updateVisualDescription()
 });
 
 function processData() {
-    timecodeArr[1] = "hello"
-    timecodeArr[3] = "text @ 3"
-    timecodeArr[4] = "text @ 4"
-    timecodeArr[5] = "text5"
-    timecodeArr[8] = "cool"
-}
-
-function getCurTime() {
     const video = document.getElementById('content')
-    document.getElementById("time").innerHTML = Math.round(video.currentTime)
-    setInterval(() => { document.getElementById("time").innerHTML = Math.round(video.currentTime) }, 1000)
+    let q = parseQuery(window.location.href.split('?')[1])
+    console.log(q)
+    let v = q["videoId"]
+    video.src = './VODs/' + v + ".mp4"
+    let o = q["option"]
+    $.getJSON('../master-data.json', (json) => {
+        for(let i = 0; i < json.length; i++) {
+            if(v == json[i].vidID && o == json[i].condition) {
+                let t = parseTime(json[i].timecode)
+                timecodeArr[t] = json[i].descTxt
+            }
+        }
+    })
 }
 
 function switchVideo() {
@@ -26,14 +31,48 @@ function switchVideo() {
 }
 
 function updateVisualDescription() {
-    let currentTime = document.getElementById("time").innerText
+    const video = document.getElementById('content')
     const desc = document.getElementById("timecodes")
-    setInterval(function () {
-        if (currentTime < timecodeArr.length && timecodeArr[currentTime]) {
-            desc.innerHTML = timecodeArr[currentTime]
+
+    document.addEventListener('desc-changed', (e) => {
+        console.log('The event happened!');
+        playSFX()
+    });
+
+    setInterval(() => {
+        document.getElementById("time").innerHTML = Math.floor(video.currentTime)
+        let currentTime = document.getElementById("time").innerText
+        let t = parseInt(currentTime)
+        if (t < timecodeArr.length && timecodeArr[t] && desc.innerHTML != timecodeArr[t]) {
+            desc.innerHTML = timecodeArr[t]
+            document.dispatchEvent(descriptionChanged)
         }
     }, 1000);
 }
 
+function parseQuery(queryString) {
+    var query = {};
+    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+    for (var i = 0; i < pairs.length; i++) {
+        var pair = pairs[i].split('=');
+        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+    }
+    return query;
+}
 // potentially add edge case for seek
 
+function playSFX() {
+    var audio = new Audio('./ringtone.mp3');
+    audio.loop = false;
+    audio.play(); 
+}
+
+function parseTime(t) {
+    const data = t.split(':')
+    if (data[0] == "NOTIME") return 0;
+    let offset = (data.length == 3) ? 1 : 0
+    const min = parseInt(data[offset])
+    const sec = parseInt(data[offset + 1])
+    const time = (min * 60) + sec
+    return time
+}
